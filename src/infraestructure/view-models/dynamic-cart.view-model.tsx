b06cb6f6fc2f570c80b8455d2c8456fc510f4933
Alejandro Mendoza <33111241+Alkhioz@users.noreply.github.com>
2023-08-01 21:31:10 -0500

    Update dynamic-cart.view-model.tsx

--- src/infraestructure/view-models/dynamic-cart.view-model.tsx
+++ src/infraestructure/view-models/dynamic-cart.view-model.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import { DynamicCartContext } from "../providers/dynamic-cart.provider"
+import { HierarchyNode } from "hierarchical-node-structure";
 
 export const UseDynamicCartViewModel = () => {
     const { store: rootNode } = useContext(DynamicCartContext);
@@ -11,11 +12,60 @@ export const UseDynamicCartViewModel = () => {
     }
 
     const addItem = (item:any) => {
-        if(!!rootNode) rootNode.addNode(item);
+        if(!!rootNode) rootNode.addNode(new HierarchyNode(item));
+    }
+
+    const addChildToItem = (id:string, item:any) => {
+        if(!!rootNode){
+            rootNode.addNodeToNode(id, new HierarchyNode(item));
+        }
+    }
+
+    const removeItem = (id: string) => {
+        if(!!rootNode){
+            rootNode.removeNodeById(id);
+        }
+    }
+
+    const getItem = (id: string) => {
+        if(!!rootNode){
+            rootNode.getNodeById(id);
+        }
+    }
+
+    const executeFunctionOnItem = (id: string, callback:any, additionalParams:any) => {
+        if(!!rootNode){
+            rootNode.runFunctionOnNode(id, callback, additionalParams);
+        }
+    }
+
+    const executeFunction = (callback:any, additionalParams:any) => {
+        if(!!rootNode){
+            rootNode.runCustomFunction(callback, additionalParams);
+        }
+    }
+
+    const getParentId = (id:string) => {
+        if(!!rootNode){
+            rootNode.getParentNodeId(id);
+        }
+    }
+
+    const traverse = (reducer:any, initialValue:any) => {
+        if(!!rootNode){
+            rootNode.traverse(reducer, initialValue);
+        }
     }
 
     return {
         getStore,
         addItem,
+        addChildToItem,
+        removeItem,
+        getItem,
+        executeFunctionOnItem,
+        executeFunction,
+        getParentId,
+        traverse,
     }
 }
